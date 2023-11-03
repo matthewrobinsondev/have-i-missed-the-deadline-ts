97fb54a5039397ed3da390d178ebb74f30a8ddc2
Matt Robinson <matthewrobinsondev@gmail.com>
2023-11-03 10:23:06 +0000

    Add logging and error handling

--- src/app/fpl/FplService.ts
+++ src/app/fpl/FplService.ts
@@ -1,5 +1,6 @@
-import { Event } from '../types/fpl/Events';
-import { Player, PlayerTransferKey } from '../types/fpl/Players';
+import { log } from "next-axiom";
+import { Event } from "../types/fpl/Events";
+import { Player, PlayerTransferKey } from "../types/fpl/Players";
 
 export class FplService {
   private fplApi: FplApiInterface;
@@ -9,46 +10,53 @@ export class FplService {
   }
 
   async getDeadline() {
-    const generalInformation = await this.fplApi.getGeneralInformation();
+    try {
+      const generalInformation = await this.fplApi.getGeneralInformation();
 
-    const currentGameweek = generalInformation.events.find(
-      (event: Event) => event.is_current
-    );
-    const nextGameweek = generalInformation.events.find(
-      (event: Event) => event.is_next
-    );
+      const currentGameweek = generalInformation.events.find(
+        (event: Event) => event.is_current
+      );
+      const nextGameweek = generalInformation.events.find(
+        (event: Event) => event.is_next
+      );
 
-    return this.calculateDeadline(currentGameweek, nextGameweek);
+      return this.calculateDeadline(currentGameweek, nextGameweek);
+    } catch (error) {
+      if (error instanceof Error) {
+        log.error(error.message);
+      }
+
+      return 0;
+    }
   }
 
   async getTopTransferredIn() {
-    return this.getTopTransferredPlayers('transfers_in_event');
+    return this.getTopTransferredPlayers("transfers_in_event");
   }
 
   async getTopTransferredOut() {
-    return this.getTopTransferredPlayers('transfers_out_event');
+    return this.getTopTransferredPlayers("transfers_out_event");
   }
 
   private async getTopTransferredPlayers(event: PlayerTransferKey) {
-    const generalInformation = await this.fplApi.getGeneralInformation();
-    const players = generalInformation.elements;
-
-    players.sort(
-      (
-        a: Player,
-        b: Player
-      ) => b[event] - a[event]
-    );
-
-    const topTransferredEvents = players
-      .slice(0, 10)
-      .map(
-        (player: Player) => player[event]
+    try {
+      const generalInformation = await this.fplApi.getGeneralInformation();
+      const players = generalInformation.elements;
+  
+      players.sort((a: Player, b: Player) => b[event] - a[event]);
+  
+      const topTransferredEvents = players
+        .slice(0, 10)
+        .map((player: Player) => player[event]);
+  
+      return players.filter((player: Player) =>
+        topTransferredEvents.includes(player[event])
       );
-
-    return players.filter((player: Player) =>
-      topTransferredEvents.includes(player[event])
-    );
+    } catch (error) {
+      if (error instanceof Error) {
+        log.error(error.message);
+      }
+    }    
   }
 
   private calculateDeadline(
