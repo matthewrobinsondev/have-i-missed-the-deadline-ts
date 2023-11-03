384b36961a39dcc0031f752ac1951a11626a41de
Matt Robinson <matthewrobinsondev@gmail.com>
2023-11-03 13:38:11 +0000

    Add public fpl route

---
+++ src/app/fpl/route.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  const res = await fetch("https://fantasy.premierleague.com/api/bootstrap-static/", {
+    next: { revalidate: 60 }, // Revalidate every 60 seconds
+  });
+  const data = await res.json();
+
+  return Response.json(data);
+}
--- src/app/third-party/FplApi.ts
+++ src/app/third-party/FplApi.ts
@@ -6,12 +6,15 @@ class FplApi implements FplApiInterface {
 
   public async getGeneralInformation(): Promise<any> {
     try {
-      const response = await this.FplAxiosInstance.get("bootstrap-static/");
-      return response.data;
+      const response = await fetch("http://localhost:3000/fpl", {
+        next: { revalidate: 300 },
+      });
+
+      return response.json();
     } catch (error) {
       if (error instanceof Error) {
         log.error(error.message);
-      }      
+      }
       throw new Error("Failed to fetch data from the FPL API.");
     }
   }
