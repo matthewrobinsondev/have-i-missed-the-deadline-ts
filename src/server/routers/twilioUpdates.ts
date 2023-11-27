c3690a52655252c5d83b76f56bef53618c82ae8c
Matt Robinson <matthewrobinsondev@gmail.com>
2023-11-27 11:29:19 +0000

    🎨 Abstract logic for future calls

--- src/server/routers/twilioUpdates.ts
+++ src/server/routers/twilioUpdates.ts
@@ -1,16 +1,16 @@
 import { TRPCError } from "@trpc/server";
 import { publicProcedure, router } from "../trpc";
-import { UserPreferences } from "@prisma/client";
+import { Prisma, PrismaClient, UserPreferences } from "@prisma/client";
 import { Player } from "@/app/types/fpl/Players";
 import { log } from "next-axiom";
+import { FplService } from "@/app/fpl/FplService";
+import TwilioApi from "@/app/third-party/TwilioApi";
+import { DefaultArgs } from "@prisma/client/runtime/library";
 
 export const TwilioUpdateRouter = router({
   updateOneDay: publicProcedure.mutation(async ({ ctx }) => {
     const oneDayTime = 86400000;
     const deadline = await ctx.fplService.getDeadline();
-    const topTransferredIn = await ctx.fplService.getTopTransferredIn();
-    const topTransferredOut = await ctx.fplService.getTopTransferredOut();
-
     const valid = isUpdateTimeValid(deadline, oneDayTime);
 
     if (!valid) {
@@ -37,30 +37,7 @@ export const TwilioUpdateRouter = router({
 
     log.info("Running cron for one day before updates.");
 
-    for (const user of users) {
-      const smsBody = getSmsBody(
-        user,
-        topTransferredIn
-          .toSorted((a, b) => b.transfers_in_event - a.transfers_in_event)
-          .slice(0, 5),
-        topTransferredOut
-          .toSorted((a, b) => b.transfers_out_event - a.transfers_out_event)
-          .slice(0, 5),
-      );
-
-      const userContact = await ctx.db.userContact.findUnique({
-        where: { user_id: user.user_id },
-      });
-
-      if (!userContact?.is_verified) {
-        log.info(`${user.user_id} does not have a verified phone number.`);
-        continue;
-      }
-
-      console.log(smsBody);
-
-      ctx.twilioApi.sendSmsUpdate(userContact.phone_number, smsBody);
-    }
+    await sendSmsUpdates(users, ctx);
 
     return `SMS updates sent.`;
   }),
@@ -73,10 +50,47 @@ function isUpdateTimeValid(deadline: number, oneDayTime: number) {
   return valid;
 }
 
+async function sendSmsUpdates(
+  users: UserPreferences[],
+  ctx: {
+    userId: string | null;
+    headers: Headers;
+    db: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+    fplService: FplService;
+    twilioApi: TwilioApi;
+  },
+) {
+  const topTransferredIn = await ctx.fplService.getTopTransferredIn();
+  const topTransferredOut = await ctx.fplService.getTopTransferredOut();
+
+  for (const user of users) {
+    const smsBody = getSmsBody(
+      user,
+      topTransferredIn
+        .toSorted((a, b) => b.transfers_in_event - a.transfers_in_event)
+        .slice(0, 5),
+      topTransferredOut
+        .toSorted((a, b) => b.transfers_out_event - a.transfers_out_event)
+        .slice(0, 5)
+    );
+
+    const userContact = await ctx.db.userContact.findUnique({
+      where: { user_id: user.user_id },
+    });
+
+    if (!userContact?.is_verified) {
+      log.info(`${user.user_id} does not have a verified phone number.`);
+      continue;
+    }
+
+    ctx.twilioApi.sendSmsUpdate(userContact.phone_number, smsBody);
+  }
+}
+
 function getSmsBody(
   user: UserPreferences,
   topTransferredIn: Player[],
-  topTransferredOut: Player[],
+  topTransferredOut: Player[]
 ): string {
   const smsBody = `🔥 Here are your FPL updates 🔥
   📈 Top Transferred In: 
@@ -85,7 +99,7 @@ function getSmsBody(
       (player, index) =>
         `${index + 1}. ${player.first_name} - ${
           player.second_name
-        }\n   Transfers In: ${player.transfers_in_event}`,
+        }\n   Transfers In: ${player.transfers_in_event}`
     )
     .join("\n")}
 
@@ -95,7 +109,7 @@ function getSmsBody(
        (player, index) =>
          `${index + 1}. ${player.first_name} - ${
            player.second_name
-         }\n   Transfers Out: ${player.transfers_out_event}`,
+         }\n   Transfers Out: ${player.transfers_out_event}`
      )
      .join("\n")}`;
 
